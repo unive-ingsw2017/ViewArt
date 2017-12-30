17601a11d7b6459ab40bb868bc52abfab443c262
Unknown <filippomaganza@gmail.com>
2017-12-30 11:42:18 +0100

    Creata Db Library e pulita Maps Activity dai metodi che non servo più

--- Datadroid-master/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/activity/MapsActivity.java
+++ Datadroid-master/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/activity/MapsActivity.java
@@ -140,11 +140,6 @@ public class MapsActivity extends AppCompatActivity
         //apri connessione con database
         db = new DbManager(this);
 
-        updateOpereaux();
-        populateAutori();
-        populateTipologie();
-        populateDate();
-
         // inizializza le preferenze
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
@@ -180,103 +175,6 @@ public class MapsActivity extends AppCompatActivity
             }
         });
     }
-
-
-
-
-
-    private void populateAutori() {
-
-        Cursor cr = MapsActivity.db.getDatabaseAccess().query(true, "opere",new String[]{AUTORE} , null,null,null,null, null, null);
-
-        cr.moveToFirst();
-        ArrayList<String> buffer = new ArrayList<>();
-        for (int i = 1; i <= cr.getCount(); i++){
-            ContentValues cv = new ContentValues();
-            cv.put(AUTORE,cr.getString(cr.getColumnIndex(AUTORE)));
-            cv.put(SELEZIONATO,0);
-            db.getDatabaseAccess().insert("autori", null, cv);
-            cr.moveToNext();
-        }
-        cr.close();
-    }
-    private void populateDate() {
-
-        Cursor cr = MapsActivity.db.getDatabaseAccess().query(true, "opere",new String[]{DATAZIONE} , null,null,null,null, null, null);
-
-        cr.moveToFirst();
-        ArrayList<String> buffer = new ArrayList<>();
-        for (int i = 1; i <= cr.getCount(); i++){
-            ContentValues cv = new ContentValues();
-            cv.put(DATA,cr.getString(cr.getColumnIndex(DATAZIONE)));
-            cv.put(SELEZIONATO,0);
-            db.getDatabaseAccess().insert("date", null, cv);
-            cr.moveToNext();
-        }
-        cr.close();
-    }
-    private void populateTipologie() {
-
-        Cursor cr = MapsActivity.db.getDatabaseAccess().query(true, "opere",new String[]{BENE_CULTURALE} , null,null,null,null, null, null);
-
-        cr.moveToFirst();
-        ArrayList<String> buffer = new ArrayList<>();
-        for (int i = 1; i <= cr.getCount(); i++){
-            ContentValues cv = new ContentValues();
-            cv.put(TIPOLOGIA,cr.getString(cr.getColumnIndex(BENE_CULTURALE)));
-            cv.put(SELEZIONATO,0);
-            db.getDatabaseAccess().insert("tipologie", null, cv);
-            cr.moveToNext();
-        }
-        cr.close();
-    }
-
-    private void updateOpereaux() {
-        Cursor cr = db.getDatabaseAccess().query(false, "opere", null , null,null,null,null, null, null);
-
-        cr.moveToFirst();
-        ArrayList<String> buffer = new ArrayList<>();
-        String autore,datazione,id;
-        for (int i = 1; i <= cr.getCount(); i++){
-            autore=cr.getString(cr.getColumnIndex(AUTORE));
-            datazione=cr.getString(cr.getColumnIndex(DATAZIONE));
-            id=cr.getString(cr.getColumnIndex(ID));
-            if (autore ==null)
-                autore="";
-            if (datazione ==null)
-                datazione="";
-
-
-
-            updateOpere(autore,datazione,id);
-            cr.moveToNext();
-        }
-        cr.close();
-
-    }
-
-    private void updateOpere(String old_autore , String old_datazione, String id) {
-        ContentValues contentValues = new ContentValues();
-        String v1=old_autore.replaceAll("[^a-zA-Z 'èéùòàì]","");
-        contentValues.put(AUTORE,v1.replaceAll("I|II|III|IV|V|VI|VII|VIII|IX|X|XI|XII|XIII|XIV|XV|XVI|XVII|XVIII|XIX|XX|XXI| notizie| sec| secc| fino| al| cc| canotizie| ca| prima| metà|  |   |   | post| capost| fine| inizio| dal| ante| notiziec]",""));
-        String s0=old_datazione.replaceAll("-","/");
-        String s1=s0.replaceAll("[^XVI/]","");
-        String s2=s1.replaceAll("  |   |    |    |      |       |        |/ |//|///|/ /","");
-        //controlla se sono secoli romani validi
-        /*if (s2.matches("(IX|IV|V?I{0,3})|(IX|IV|V?I{0,3})/(IX|IV|V?I{0,3})")==true)
-            contentValues.put(DATAZIONE,s2 + " Secolo");
-        else
-            contentValues.put(DATAZIONE,"Data assente o non valida");*/
-        contentValues.put(DATAZIONE,s2 + " Secolo");
-
-        db.getDatabaseAccess().update( "opere",contentValues ,(ID + " =" + id),null);
-    }
-
-
-
-
-
-
 
     // ciclo di vita della app
     @Override
